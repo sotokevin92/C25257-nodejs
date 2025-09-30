a731bfe3463de230fdfcf0b0beea3f547616030f
Jazzy <sotokevin92@gmail.com>
2025-09-30 20:37:48 -0300

    Clase 6, misión 2: uso de async-await y try-catch-finally

--- clase6-async/api.js
+++ clase6-async/api.js
@@ -1,14 +1,26 @@
 const url = 'https://rickandmortyapi.com/api/character';
 
-console.log('Mostrando los primeros 5 resultados de la consulta de personajes:');
+async function ejecutarConsulta() {
+    const resultadoConsulta = await fetch(url);
+    if (!resultadoConsulta.ok) {
+        throw new Error('No se pudo obtener datos de la consulta a la API.');
+    }
 
-fetch(url).then(data => data.json())
-    .then(data => data.results)
-    .then(personajes => personajes.slice(0, 5)
-        .forEach(personaje => console.log(`${personaje.name} (${personaje.species}, ${personaje.gender})`))
-    ).catch(error => console.log('¡Error consultando la API!', error))
-    .finally(() => {
-        console.log();
-        console.log('Consulta finalizada.');
-        console.log();
-    });
+    const personajes = (await resultadoConsulta.json()).results;
+    return personajes.slice(0, 5);
+}
+
+try {
+    console.log('Mostrando los primeros 5 resultados de la consulta de personajes:');
+
+    const personajes = await ejecutarConsulta();
+    personajes.forEach(personaje =>
+        console.log(`${personaje.name} (${personaje.species}, ${personaje.gender})`)
+    );
+} catch (error) {
+    console.error('¡Error consultando la API!', error);
+} finally{
+    console.log();
+    console.log('Consulta finalizada.');
+    console.log();
+}
